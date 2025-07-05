813fd790de4eef58e160308396f8a4989b0f999d
Mushary Rilwan <musharymushary72@gmail.com>
2025-07-06 01:28:58 +0530

    chat view fix

--- frontend/src/pages/chatbot/Chatbot.tsx
+++ frontend/src/pages/chatbot/Chatbot.tsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from "react"
 import { useSearchParams } from "react-router-dom"
 import InputBar from "../../components/InputBar"
 import type { IChatMessageResponseBody } from "../../lib/types/chatbot/chatMessage"
+import { useLoadChatMessagesBySessionId } from "../../services/chatbot/queries"
 import ChatView from "./ChatView"
 import InitialChatView from "./InitialChatView"
 
 const Chatbot = () => {
   const [searchParams] = useSearchParams()
   const sessionId = searchParams.get("session_id")
+
+  const { data: chatMessages } = useLoadChatMessagesBySessionId(sessionId ?? "")
 
   const [optimisticMessages, setOptimisticMessages] = useState<
     IChatMessageResponseBody[]
@@ -28,19 +31,29 @@ const Chatbot = () => {
             isSendingMessage={isSendingMessage}
           />
         </div>
+      ) : sessionId && chatMessages?.length === 0 ? (
+        <>
+          <ChatView
+            optimisticMessages={optimisticMessages}
+            sessionId={sessionId}
+            setOptimisticMessages={setOptimisticMessages}
+            isSendingMessage={isSendingMessage}
+            setIsSendingMessage={setIsSendingMessage}
+          />
+        </>
       ) : (
         <>
           <div className="flex-1 overflow-hidden">
             <ChatView
               optimisticMessages={optimisticMessages}
-              sessionId={sessionId ?? ""}
+              sessionId={sessionId}
               setOptimisticMessages={setOptimisticMessages}
               isSendingMessage={isSendingMessage}
               setIsSendingMessage={setIsSendingMessage}
             />
           </div>
           <InputBar
-            sessionId={sessionId ?? ""}
+            sessionId={sessionId}
             setOptimisticMessages={setOptimisticMessages}
             setIsSendingMessage={setIsSendingMessage}
             isSendingMessage={isSendingMessage}
